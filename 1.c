ed5a8532288773c2c10b9c606cda003387130bb1
mzhhaoo <mzhhaoo@foxmail.com>
2014-12-01 16:49:33 +0800

    add 1.c

---
+++ 1.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  1.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  12/01/2014 04:47:47 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	printf("hel\n");
+}
